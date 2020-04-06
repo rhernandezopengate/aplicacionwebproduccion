284f624a4aafab5a1f0af083f1f710f48419d203
rhernandezopengate <43422446+rhernandezopengate@users.noreply.github.com>
2020-04-06 09:05:39 -0500

    Primer Commit

---
+++ WebAppProduccion/WebAppProduccion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppProduccion.Startup))]
+namespace WebAppProduccion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
